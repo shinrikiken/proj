674d4ef0dd2d6a244a658ed4250f2942cc1b2623
Supachai <Supachai>
2017-06-01 10:55:14 +0700

    set up new project

---
+++ proj1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proj1
+//
+//  Created by Supachai Abusali on 6/1/17.
+//  Copyright © 2017 Supachai Abusali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
